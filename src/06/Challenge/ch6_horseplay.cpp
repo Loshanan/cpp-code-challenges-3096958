7fb211b4ccde0765e9d71dc00ae53bcdc95c44b0
Loshanan <76877774+Loshanan@users.noreply.github.com>
2024-07-11 04:51:43 +0000

    challenge 6 done

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 // knight_moves()
 // Summary: This function receives a string with the location of a knight in a chessboard and returns a vector of strings with the possible locations it might move to.
@@ -17,8 +18,34 @@
 // Returns: An STL vector of strings with the possible locations to move.
 std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
+    std::transform(knight.begin(), knight.end(), knight.begin(), tolower);
 
     // Write your code here
+    int col = int(knight[0]) - 96;
+    int row = knight[1] - '1' +1;  // works only for single degit
+    std::string temp;
+
+    int delta1[2] = {-1, 1};
+    int delta2[2] = {-2, 2};
+
+    for (int i: delta1){
+        for (int j: delta2){
+            if (col+i <= 8 && col+i >=1 && row+j <= 8 && row+j>=1){
+                temp += char(col+i+96);
+                temp += char(row+j + int('0'));
+                moves.push_back(temp);
+                temp = "";
+            }
+            if (col+j <= 8 && col+j >=1 && row+i <= 8 && row+i>=1){
+                temp += char(col+j+96);
+                temp += char(row+i + int('0'));
+                moves.push_back(temp);
+                temp = "";
+            }
+        }
+    }
+
+
 
     return moves;
 }
